fbad5a1eb69cd823820fcdbd8fc6eb9d6a8953d0
dillanbarbosa <dillan.invencao@escola.pr.gov.br>
2026-05-07 11:26:33 -0300

    Add files via upload

--- PDP.js
+++ PDP.js
@@ -6,27 +6,26 @@ async function cafe() {
         const response = await fetch('PDP.json');
 
         if (!response.ok) throw new Error('Falha ao carregar dados');
-        
+
         const cafes = await response.json();
-         
+
         destino.innerHTML = '';
-        
+
         cafes.forEach(item => {
             const prod = document.createElement('div');
             prod.className = 'model';
-            
-            prod.innerHTML = `
-                <img src="imagens/${item.imagem}" alt="Foto de ${item.nome}">
-                <h3></h3>
-                <p></p>
-                <a href="${item.link}">
-                <button type="button">Saiba Mais</button>
-                </a>`;
 
-            prod.querySelector('h3').textContent = item.nome;
+            prod.innerHTML = `
+                <img src="Imgs/${item.imagem}" class="imagem_produto">
+                <p class="d_produto">${item.nome}</p>
+                <p class="p_produto">${item.preco}</p>
+                <a href="${item.link}" class="botao_produto">Saiba Mais</a>`;
+
+            prod.querySelector('.d_produto').textContent = item.nome;
+            prod.querySelector('.p_produto').textContent = item.preco;
             destino.appendChild(prod);
         });
-    }        catch (error) {
+    } catch (error) {
         console.error('Erro na requisição:', error);
         destino.innerHTML = `
         <p>Desculpa, não foi possível carregar as informações no momento.</p>
